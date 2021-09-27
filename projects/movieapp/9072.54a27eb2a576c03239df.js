20a333ff0d65813dde6918c4c2fec4c48f4d0839
isaacW <ISAAC.WILSON1811@STU.MTEC.EDU>
2021-09-27 13:05:01 -0600

    prod build

---
+++ projects/movieapp/9072.54a27eb2a576c03239df.js
@@ -0,0 +1,1 @@
+(self.webpackChunkmovieApp=self.webpackChunkmovieApp||[]).push([[9072],{9072:(o,s,r)=>{"use strict";r.r(s),r.d(s,{ion_text:()=>l});var t=r(3150),c=r(7585),e=r(1269);const l=class{constructor(o){(0,t.r)(this,o)}render(){const o=(0,c.b)(this);return(0,t.h)(t.H,{class:(0,e.c)(this.color,{[o]:!0})},(0,t.h)("slot",null))}};l.style=":host(.ion-color){color:var(--ion-color-base)}"}}]);
